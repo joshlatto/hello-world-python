8d01c35fbaf0544286cb54230e729c56dffd9e4b
Josh Latto <josh.latto@gmail.com>
2025-08-05 19:18:04 -0700

    Added a find_prime_factors function as an example.

--- hello-world-python.py
+++ hello-world-python.py
@@ -1,1 +1,18 @@
 print("Hello world!")
+
+def find_prime_factors(n):
+    factors = []
+    divisor = 2
+    while n > 1:
+        print("Current number:", n)
+        print("Divisor:", divisor)
+        while n % divisor == 0:
+            factors.append(divisor)
+            print(factors)
+            n //= divisor
+            print("Current number:", n)
+            print("Divisor:", divisor)
+        divisor += 1
+    return factors
+
+print("Prime factors of 56:", find_prime_factors(56))
